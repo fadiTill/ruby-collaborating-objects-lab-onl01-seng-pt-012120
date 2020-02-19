447c3a435579b959a5e7f7a6cc798fe204c30bbf
fadimata tillman <fadi.tillman@gmail.com>
2020-02-19 04:39:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,16 @@
 
 class Song
-
+ @@all[]
  attr_accessor :name, :artist
 
  def initialize(name)
    @name = name
+   @@all << self
  end
+
+def songs_list
+  @@all
+end
 
  def self.new_by_filename(file_name)
    song = file_name.split(" - ")[1]
