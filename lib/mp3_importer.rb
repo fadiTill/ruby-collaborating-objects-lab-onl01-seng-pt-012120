99710e9cde605c051e630424284b8390612e2631
fadimata tillman <fadi.tillman@gmail.com>
2020-02-19 04:26:53 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,10 @@
-# class MP3Importer
-#
-#   attr_accessor :path
-#
-#   def initialize(path)
-#     @path = path
-#   end
+ class MP3Importer
+
+  attr_accessor :path
+
+   def initialize(path)
+     @path = path
+   end
 #
 #   def files
 #     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
